5dd56fee8a5255f86b32556186bcbc4102b5d039
王海山 <wanghaishan@geekplus.com.cn>
2020-02-13 16:10:13 +0800

    交易地图还两点数据模拟

--- src/components/ChartDeal.vue
+++ src/components/ChartDeal.vue
@@ -1,28 +1,10 @@
 <template>
-  <baidu-map :zoom="zoom" :center="center" @ready="handleReady" style="height:100%">
-    <bm-marker :position="center" />
-    <bm-control>
-    </bm-control>
-  </baidu-map>
+  <div id="multiMap" style="width:100%;height:100%;"></div>
 </template>
 
 <script>
 import * as api from '@/api'
 export default {
-  props: {
-    lng: {
-      type: Number,
-      default: 116.232922
-    },
-    lat: {
-      type: Number,
-      default: 39.542637
-    },
-    zoom: {
-      type: Number,
-      default: 15
-    }
-  },
   data () {
     return {
       map: null,
@@ -42,81 +24,83 @@ export default {
     }
   },
   mounted () {
+    this.getMapScript().then(BMap => {
+      var map = new BMap.Map('multiMap', {}) // 创建Map实例
+      map.centerAndZoom(new BMap.Point(112.0498046875, 27.1484375), 8) // 初始化地图,设置中心点坐标和地图级别
+      map.enableScrollWheelZoom() // 启用滚轮放大缩小
+      if (document.createElement('canvas').getContext) { // 判断当前浏览器是否支持绘制海量点
+      // 接口加载
+        api.getAllMchInfoList().then(res => {
+          const points = []
+          var i = 0
+          for (;i < res.data.length; i++) {
+            points.push(new BMap.Point(res.data[i].x, res.data[i].y))// 创建坐标点
+          }
+          var options = {
+            size: BMAP_POINT_SIZE_SMALL,
+            shape: BMAP_POINT_SHAPE_CIRCLE,
+            color: 'red'
+          }
+          var pointCollection = new BMap.PointCollection(points, options) // 初始化PointCollection
+          pointCollection.addEventListener('click', function (e) {
+            alert('单击点的坐标为：' + e.point.lng + ',' + e.point.lat) // 监听点击事件
+          })
+          map.addOverlay(pointCollection) // 添加Overlay
+        })
+
+        // 假数据模拟
+        // const points = []
+        // var mapPoints = [
+        //   { y: 27.82, x: 112.55 },
+        //   { y: 27.73, x: 111.5 },
+        //   { y: 26.13, x: 111.6 },
+        //   { y: 26.9, x: 112.6 },
+        //   { y: 28.19, x: 112.6 },
+        //   { x: 109.98804490894351, y: 27.54911215611929 },
+        //   { x: 109.98988774833771, y: 27.557991094785056 },
+        //   { x: 110.14803797676342, y: 27.325122562561397 }
+        // ]
+        // var i = 0
+        // for (;i < mapPoints.length; i++) {
+        //   points.push(new BMap.Point(mapPoints[i].x, mapPoints[i].y))// 创建坐标点
+        // }
+        // var options = {
+        //   size: BMAP_POINT_SIZE_SMALL,
+        //   shape: BMAP_POINT_SHAPE_CIRCLE,
+        //   color: 'red'
+        // }
+        // var pointCollection = new BMap.PointCollection(points, options) // 初始化PointCollection
+        // pointCollection.addEventListener('click', function (e) {
+        //   alert('单击点的坐标为：' + e.point.lng + ',' + e.point.lat) // 监听点击事件
+        // })
+        // map.addOverlay(pointCollection) // 添加Overlay
+      } else {
+        alert('请在chrome、safari、IE8+以上浏览器查看本示例')
+      }
+    })
   },
   methods: {
-    handleReady ({ BMap, map }) {
-      this.map = map
-      this.BMap = BMap
-      map.addControl(new BMap.NavigationControl({
-        anchor: global.BMAP_ANCHOR_TOP_RIGHT
-      }))
-      this.setMarkerExt()
-    },
-    setMarkerExt () {
-      const { BMap } = this
-      console.log(this)
-
-      // 接口请求海量标点
-      this.map.enableScrollWheelZoom(true)
-      var point = new BMap.Point('112.0498046875', '27.1484375') // 定位的地图视觉中心
-      this.map.centerAndZoom(point, 7) // 设置地图的缩放级别
-
-      api.getAllMchInfoList().then(res => {
-        const points = []
-        var i = 0
-        for (;i < res.data.length; i++) {
-          points.push(new BMap.Point(res.data[i].x, res.data[i].y))// 创建坐标点
-        }
-        const options = {
-          size: 13,
-          shape: 3,
-          color: '#009900'
-        }
-        const pointCollection = new BMap.PointCollection(points, options)
-        this.map.addOverlay(pointCollection)
-      })
-      // 加载海量点《模拟》数据
-      // const ggPoint = new BMap.Point('112.0498046875', '27.1484375')
-      // this.map.centerAndZoom(ggPoint, 7) // 控制地图大小比例
-      // const points = []
-      // var mapPoints = [
-      //   { y: 27.82, x: 112.55 },
-      //   { y: 27.73, x: 111.5 },
-      //   { y: 26.13, x: 111.6 },
-      //   { y: 26.9, x: 112.6 },
-      //   { y: 28.19, x: 112.6 },
-      //   { x: 109.98804490894351, y: 27.54911215611929 },
-      //   { x: 109.98988774833771, y: 27.557991094785056 },
-      //   { x: 110.14803797676342, y: 27.325122562561397 }
-      // ]
-      // var i = 0
-      // for (;i < mapPoints.length; i++) {
-      //   points.push(new BMap.Point(mapPoints[i].x, mapPoints[i].y))// 创建坐标点
-      // }
-      // const options = {
-      //   size: 13,
-      //   shape: 3,
-      //   color: '#009900'
-      // }
-      // const pointCollection = new BMap.PointCollection(points, options)
-      // this.map.addOverlay(pointCollection)
-      // 加载多个点
-      // var mapPoints = [
-      //   { y: 27.82, x: 112.55 },
-      //   { y: 27.73, x: 111.5 },
-      //   { y: 26.13, x: 111.6 },
-      //   { y: 26.9, x: 112.6 },
-      //   { y: 28.19, x: 112.6 },
-      //   { x: 109.98804490894351, y: 27.54911215611929 },
-      //   { x: 109.98988774833771, y: 27.557991094785056 },
-      //   { x: 110.14803797676342, y: 27.325122562561397 }
-      // ]
-      // var i = 0
-      // for (;i < mapPoints.length; i++) {
-      //   var points = new BMap.Point(mapPoints[i].x, mapPoints[i].y)// 创建坐标点
-      // var markers = new BMap.Marker(points)
-      // this.map.addOverlay(markers)
-      // }
+    getMapScript () {
+      if (!global.BMap) {
+        const ak = 'O6GKe9A0L4xThRNPHotMrd5YpYlKotDY'
+        global.BMap = {}
+        global.BMap._preloader = new Promise((resolve, reject) => {
+          global._initBaiduMap = function () {
+            resolve(global.BMap)
+            global.document.body.removeChild($script)
+            global.BMap._preloader = null
+            global._initBaiduMap = null
+          }
+          const $script = document.createElement('script')
+          global.document.body.appendChild($script)
+          $script.src = `https://api.map.baidu.com/api?v=2.0&ak=${ak}&callback=_initBaiduMap`
+        })
+        return global.BMap._preloader
+      } else if (!global.BMap._preloader) {
+        return Promise.resolve(global.BMap)
+      } else {
+        return global.BMap._preloader
+      }
     }
   }
 }
---
+++ src/components/ChartDeal1.vue
@@ -0,0 +1,126 @@
+<template>
+  <baidu-map :zoom="zoom" :center="center" @ready="handleReady" style="height:100%">
+    <bm-marker :position="center" />
+    <bm-control>
+    </bm-control>
+  </baidu-map>
+</template>
+
+<script>
+// import * as api from '@/api'
+export default {
+  props: {
+    lng: {
+      type: Number,
+      default: 116.232922
+    },
+    lat: {
+      type: Number,
+      default: 39.542637
+    },
+    zoom: {
+      type: Number,
+      default: 15
+    }
+  },
+  data () {
+    return {
+      map: null,
+      center: {}
+    }
+  },
+  watch: {
+    lng () {
+      if (this.map) {
+        this.setMarker(1)
+      }
+    },
+    lat () {
+      if (this.map) {
+        this.setMarker(1)
+      }
+    }
+  },
+  mounted () {
+  },
+  methods: {
+    handleReady ({ BMap, map }) {
+      this.map = map
+      this.BMap = BMap
+      map.addControl(new BMap.NavigationControl({
+        anchor: global.BMAP_ANCHOR_TOP_RIGHT
+      }))
+      this.setMarkerExt()
+    },
+    setMarkerExt () {
+      const { BMap } = this
+      console.log(this)
+
+      // 接口请求海量标点
+      // this.map.enableScrollWheelZoom(true)
+      // var point = new BMap.Point('112.0498046875', '27.1484375') // 定位的地图视觉中心
+      // this.map.centerAndZoom(point, 7) // 设置地图的缩放级别
+
+      // api.getAllMchInfoList().then(res => {
+      //   const points = []
+      //   var i = 0
+      //   for (;i < res.data.length; i++) {
+      //     points.push(new BMap.Point(res.data[i].x, res.data[i].y))// 创建坐标点
+      //   }
+      //   const options = {
+      //     size: 13,
+      //     shape: 3,
+      //     color: '#009900'
+      //   }
+      //   const pointCollection = new BMap.PointCollection(points, options)
+      //   this.map.addOverlay(pointCollection)
+      // })
+      // 加载海量点《模拟》数据
+      const ggPoint = new BMap.Point('112.0498046875', '27.1484375')
+      this.map.centerAndZoom(ggPoint, 7) // 控制地图大小比例
+      const points = []
+      var mapPoints = [
+        { y: 27.82, x: 112.55 },
+        { y: 27.73, x: 111.5 },
+        { y: 26.13, x: 111.6 },
+        { y: 26.9, x: 112.6 },
+        { y: 28.19, x: 112.6 },
+        { x: 109.98804490894351, y: 27.54911215611929 },
+        { x: 109.98988774833771, y: 27.557991094785056 },
+        { x: 110.14803797676342, y: 27.325122562561397 }
+      ]
+      var i = 0
+      for (;i < mapPoints.length; i++) {
+        points.push(new BMap.Point(mapPoints[i].x, mapPoints[i].y))// 创建坐标点
+      }
+      const options = {
+        size: 13,
+        shape: 3,
+        color: '#009900'
+      }
+      const pointCollection = new BMap.PointCollection(points, options)
+      this.map.addOverlay(pointCollection)
+      // 加载多个点
+      // var mapPoints = [
+      //   { y: 27.82, x: 112.55 },
+      //   { y: 27.73, x: 111.5 },
+      //   { y: 26.13, x: 111.6 },
+      //   { y: 26.9, x: 112.6 },
+      //   { y: 28.19, x: 112.6 },
+      //   { x: 109.98804490894351, y: 27.54911215611929 },
+      //   { x: 109.98988774833771, y: 27.557991094785056 },
+      //   { x: 110.14803797676342, y: 27.325122562561397 }
+      // ]
+      // var i = 0
+      // for (;i < mapPoints.length; i++) {
+      //   var points = new BMap.Point(mapPoints[i].x, mapPoints[i].y)// 创建坐标点
+      // var markers = new BMap.Marker(points)
+      // this.map.addOverlay(markers)
+      // }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
