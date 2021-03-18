d360bc33e2a28bc65d98b0e44ed4af94120c307b
lxyc <tech_xyc@163.com>
2021-03-18 23:17:07 +0800

    交易视图

--- src/components/transactionMonitorChart.vue
+++ src/components/transactionMonitorChart.vue
@@ -2,9 +2,15 @@
   <div class="monitor-chart position-space">
     <div class="title">
       <div class="tab">
-        <div class="item flex-center" :class="isChart ? 'active':''" @click="chartClick">月交易量</div>
-        <div class="item flex-center" :class="isGrade ? 'active':''" @click="gradeClick">地市交易</div>
-        <div class="item flex-center" :class="isActivity ? 'active':''"  @click="activityClick">交易种类分布</div>
+        <div class="item flex-center" :class="isChart ? 'active' : ''" @click="chartClick">
+          月交易量
+        </div>
+        <div class="item flex-center" :class="isGrade ? 'active' : ''" @click="gradeClick">
+          地市交易
+        </div>
+        <div class="item flex-center" :class="isActivity ? 'active' : ''" @click="activityClick">
+          交易种类分布
+        </div>
       </div>
     </div>
     <!-- 交易笔数柱状图 -->
@@ -27,7 +33,7 @@ import * as api from '@/api'
 import echarts from 'echarts'
 
 export default {
-  data () {
+  data() {
     return {
       isChart: true,
       isGrade: false,
@@ -48,12 +54,12 @@ export default {
   },
   methods: {
     // 交易笔数交易金额
-    chartClick () {
+    chartClick() {
       // const list = { 'dataAxis': ['长沙', '株洲', '湘潭', '衡阳', '邵阳', '岳阳', '常德', '张家界', '益阳', '郴州', '永州', '怀化', '自治州', '娄底'], 'data2': ['143193589', '210927236', '165573746', '148498393', '399551593', '293959838', '410724505', '186772210', '143527674', '171968171', '756672763', '842089629', '246434172', '163790509'], 'data1': ['1174286', '1087766', '724476', '1193744', '1717996', '1400677', '2002448', '2101917', '588114', '1397801', '7784269', '5677594', '1093692', '1185870'] }
       let classType = {
         reportPlace: 311
       }
-      api.getClassInfo(classType).then(res => {
+      api.getClassInfo(classType).then((res) => {
         this.isActivity = false
         this.isChart = true
         this.isGrade = false
@@ -75,7 +81,7 @@ export default {
       //   this.chartInit()
       // }, 500)
     },
-    chartInit () {
+    chartInit() {
       let myChart = echarts.init(document.getElementById('line-chart'))
       let option = {
         color: ['#22E7E4', '#EE7343'],
@@ -89,7 +95,8 @@ export default {
         // },
         tooltip: {
           trigger: 'axis',
-          axisPointer: { // 坐标轴指示器，坐标轴触发有效
+          axisPointer: {
+            // 坐标轴指示器，坐标轴触发有效
             type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
           }
         },
@@ -140,7 +147,8 @@ export default {
               color: '#22E7E4',
               fontWeight: 'bold'
             }
-          }, {
+          },
+          {
             name: '交易金额(万元)',
             type: 'value',
             scale: true,
@@ -172,7 +180,8 @@ export default {
             // name: '交易笔数(笔)',
             type: 'bar',
             barWidth: '15%',
-            itemStyle: { // 柱样式
+            itemStyle: {
+              // 柱样式
               normal: {
                 barBorderRadius: [10, 10, 10, 10]
               }
@@ -184,7 +193,8 @@ export default {
             // name: '交易金额(万元)',
             type: 'bar',
             barWidth: '15%',
-            itemStyle: { // 柱样式
+            itemStyle: {
+              // 柱样式
               normal: {
                 barBorderRadius: [10, 10, 10, 10]
               }
@@ -196,11 +206,11 @@ export default {
       }
       myChart.setOption(option, true)
     },
-    gradeClick () {
+    gradeClick() {
       let classType = {
         reportPlace: 321
       }
-      api.getClassInfo(classType).then(res => {
+      api.getClassInfo(classType).then((res) => {
         this.gradeList = res.data.dataAxis
         this.gradeStrokeCount = res.data.data1
         this.gradeNum = res.data.data2
@@ -219,7 +229,7 @@ export default {
       // }, 500)
     },
     // 等级
-    grade () {
+    grade() {
       let myChart = echarts.init(document.getElementById('line-grade'))
       let option = {
         color: ['#22E7E4', '#EE7343'],
@@ -244,7 +254,8 @@ export default {
         },
         tooltip: {
           trigger: 'axis',
-          axisPointer: { // 坐标轴指示器，坐标轴触发有效
+          axisPointer: {
+            // 坐标轴指示器，坐标轴触发有效
             type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
           }
         },
@@ -295,7 +306,8 @@ export default {
               color: '#22E7E4',
               fontWeight: 'bold'
             }
-          }, {
+          },
+          {
             name: '交易金额(万元)',
             type: 'value',
             scale: true,
@@ -327,7 +339,8 @@ export default {
             name: '',
             type: 'bar',
             barWidth: '15%',
-            itemStyle: { // 柱样式
+            itemStyle: {
+              // 柱样式
               normal: {
                 barBorderRadius: [10, 10, 10, 10]
               }
@@ -339,7 +352,8 @@ export default {
             name: '',
             type: 'bar',
             barWidth: '15%',
-            itemStyle: { // 柱样式
+            itemStyle: {
+              // 柱样式
               normal: {
                 barBorderRadius: [10, 10, 10, 10]
               }
@@ -352,11 +366,11 @@ export default {
       myChart.setOption(option, true)
     },
     // 营销活动
-    activityClick () {
+    activityClick() {
       let classType = {
         reportPlace: 331
       }
-      api.getClassInfo(classType).then(res => {
+      api.getClassInfo(classType).then((res) => {
         this.activityList = res.data.dataAxis
         this.activityStrokeCount = res.data.data1
         this.activityNum = res.data.data2
@@ -374,151 +388,79 @@ export default {
       //   this.activity()
       // }, 500)
     },
-    activity () {
+    activity() {
       let myChart = echarts.init(document.getElementById('line-activity'))
       let option = {
-        color: ['#22E7E4', '#EE7343'],
-        // title: {
-        //   left: '30%',
-        //   text: '2018年7月到2019年6月',
-        //   textStyle: {
-        //     color: '#ffffff',
-        //     fontSize: 8
-        //   }
-        // },
-        legend: {
-          // data: ['交易笔数(笔)', '交易金额(万元)'],
-          position: 'absolute',
-          align: 'left', // 水平方向位置
-          verticalAlign: 'top', // 垂直方向位置
-          x: 100, // 距离x轴的距离
-          y: 0, // 距离Y轴的距离
-          textStyle: {
-            color: '#ffffff'
-          }
-        },
-        tooltip: {
-          trigger: 'axis',
-          axisPointer: { // 坐标轴指示器，坐标轴触发有效
-            type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
-          }
-        },
-        grid: {
-          top: '20%',
-          left: '1%',
-          right: '1%',
-          bottom: '',
-          containLabel: true
-        },
-        xAxis: [
-          {
-            type: 'category',
-            data: this.activityList,
-            axisTick: {
-              alignWithLabel: true,
-              lineStyle: { color: '#D2FBFE' } // x轴刻度的颜色
-            },
-            axisLabel: { color: '#D2FBFE',
-              interval: 0,
-              formatter: function (value, index) {
-                if (index % 2 !== 0) {
-                  return '\n\n' + value
-                } else {
-                  return value
-                }
-              } }, // x轴字体颜色
-            axisLine: {
-              lineStyle: { color: '#CAF7FF' } // x轴坐标轴颜色
-            }
-          }
-        ],
-        yAxis: [
-          {
-            name: '交易笔数(笔)',
-            type: 'value',
-            max: 1300000,
-            min: 0,
-            axisLabel: { color: '#D2FBFE' }, // y轴字体颜色
-            axisLine: {
-              show: false,
-              lineStyle: {
-                color: '#D2FBFE'
-              }
-            },
-            axisTick: {
-              show: false
-            },
-            splitLine: {
-              lineStyle: {
-                color: 'rgba(202,247,255,0.51)'
-              }
-            },
-            nameTextStyle: {
-              color: '#22E7E4',
-              fontWeight: 'bold'
-            }
-          }, {
-            name: '交易金额(万元)',
-            type: 'value',
-            max: 500000,
-            min: 0,
-            axisLabel: { color: '#D2FBFE' }, // y轴字体颜色
-            axisLine: {
-              show: false,
-              lineStyle: {
-                color: '#EE7343'
-              }
-            },
-            axisTick: {
-              show: false
-            },
-            splitLine: {
-              lineStyle: {
-                color: 'rgba(202,247,255,0.51)'
-              }
-            },
-            nameTextStyle: {
-              color: '#EE7343',
-              fontWeight: 'bold'
-            }
-          }
-        ],
+        color: ['#f6da22', '#bbe2e8', '#6cacde', '#00FFF0', '#feb901'],
         series: [
           {
-            name: '',
-            type: 'bar',
-            barWidth: '20%',
-            itemStyle: { // 柱样式
+            type: 'pie',
+            radius: ['45%', '60%'],
+            center: ['25%', '50%'],
+            data: this.activityList.map((e, i) => ({
+              name: e,
+              value: this.activityStrokeCount[i]
+            })),
+            hoverAnimation: false,
+            label: {
               normal: {
-                barBorderRadius: [10, 10, 10, 10]
+                formatter: (params) => {
+                  return '{name|' + params.name + '}{value|' + params.value + '}'
+                },
+                rich: {
+                  name: {
+                    fontSize: 14,
+                    padding: [0, 10, 0, 4],
+                    color: '#fff'
+                  },
+                  value: {
+                    fontSize: 16,
+                    fontWeight: 'bold',
+                    color: '#fff'
+                  }
+                }
               }
-            },
-            yAxisIndex: 0,
-            data: this.activityStrokeCount
+            }
           },
           {
-            name: '',
-            type: 'bar',
-            barWidth: '20%',
-            itemStyle: { // 柱样式
+            type: 'pie',
+            radius: ['45%', '60%'],
+            center: ['75%', '50%'],
+            data: this.activityList.map((e, i) => ({
+              name: e,
+              value: this.activityNum[i]
+            })),
+            hoverAnimation: false,
+            label: {
               normal: {
-                barBorderRadius: [10, 10, 10, 10]
+                formatter: (params) => {
+                  return '{name|' + params.name + '}{value|' + params.value + '}'
+                },
+                rich: {
+                  name: {
+                    fontSize: 14,
+                    padding: [0, 10, 0, 4],
+                    color: '#fff'
+                  },
+                  value: {
+                    fontSize: 16,
+                    fontWeight: 'bold',
+                    color: '#fff'
+                  }
+                }
               }
-            },
-            yAxisIndex: 1,
-            data: this.activityNum
+            }
           }
         ]
       }
       myChart.setOption(option, true)
     }
-
   },
-  mounted () {
+  mounted() {
     let classType = {
       reportPlace: 311
     }
-    api.getClassInfo(classType).then(res => {
+    api.getClassInfo(classType).then((res) => {
       this.chartList = res.data.dataAxis
       this.chartStrokeCount = res.data.data1
       this.chartNum = res.data.data2
@@ -532,7 +474,9 @@ export default {
 @import '../assets/style/index.scss';
 .monitor-chart {
   .chart-wrapper {
-    #line-chart, #line-grade, #line-activity {
+    #line-chart,
+    #line-grade,
+    #line-activity {
       height: 100%;
     }
   }
